a949e2b50fd27279ac693de211555cb97f280940
gabrielrcosta1 <contatogrsoftwares@gmail.com>
2024-11-08 07:44:36 -0400

    wip

---
+++ app/Http/Controllers/Api/JobVacancyController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\JobVacancy;
+use Illuminate\Http\Request;
+
+class JobVacancyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobVacancy $jobVacancy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JobVacancy $jobVacancy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobVacancy $jobVacancy)
+    {
+        //
+    }
+}
